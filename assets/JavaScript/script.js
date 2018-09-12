c0bbfa3d84380fe298e73b7804633dc0beb6f443
unknown <jayng168@gmail.com>
2018-09-12 16:59:24 -0400

    Update

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -42,56 +42,56 @@ $(document).ready(function () {
     $("#startYear").val("");
     $("#endYear").val("");
 
-    for (var i = 0; i < pageNum ; i++){
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
-      'api-key': "29d76fe43ea8461e9ce391c77e04cfc4",
-      'q': searchTerm,
-      'begin_date': startYear + "0101",
-      'end_date': endYear + "1231",
-      'page': i
+    for (var i = 0; i < pageNum; i++) {
+      var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+      url += '?' + $.param({
+        'api-key': "29d76fe43ea8461e9ce391c77e04cfc4",
+        'q': searchTerm,
+        'begin_date': startYear + "0101",
+        'end_date': endYear + "1231",
+        'page': i
+      });
+
+      $.ajax({
+        url: url,
+        method: 'GET',
+      }).done(function (result) {
+        console.log(result);
+
+        var res = result.response.docs
+
+        for (var j = 0; j <= 10; j++) {
+
+          count++
+
+          $("#searchResults").append(
+
+            "<br> <div id='entry' class='p-2 rounded' href=" + res[j].web_url + "> Count: " + count +
+
+            "<br> <h2 id='entryHead'>" + res[j].headline.main + "</h2>" +
+
+            "<br> <p id='entryBody'>" + res[j].snippet + "</p>" +
+
+            "<br> <p id='entryLink'> Link: <a target='_blank' href=" + res[j].web_url + ">" + res[j].web_url + "</a> </p> </div><hr>"
+
+            // "<br><img src=" + res[i].multimedia.url + ">"
+
+          )
+
+        };
+
+
+      }).fail(function (err) {
+        throw err;
+      });
+    };
+
+    $("#entry").on("click", function () {
+      console.log("got clicked");
+      // window.location = $(this).find("a").attr("href"); 
+      window.open("https://www.google.com/");
+      // window.open($(this).attr("href"), '_blank');
     });
-
-    $.ajax({
-      url: url,
-      method: 'GET',
-    }).done(function (result) {
-      console.log(result);
-
-      var res = result.response.docs
-
-      for (var j = 0; j <= 10; j++) {
-
-        count++
-
-        $("#searchResults").append(
-
-          "<br> <div id='entry' class='p-2 rounded' href=" + res[j].web_url + "> Count: " + count +
-
-          "<br> <h2 id='entryHead'>" + res[j].headline.main + "</h2>" +
-
-          "<br> <p id='entryBody'>" + res[j].snippet + "</p>" +
-
-          "<br> <p id='entryLink'> Link: <a target='_blank' href=" + res[j].web_url + ">" + res[j].web_url + "</a> </p> </div><hr>"
-
-          // "<br><img src=" + res[i].multimedia.url + ">"
-
-        )
-
-      };
-      
-     
-    }).fail(function (err) {
-      throw err;
-    });
-  };
-
- $("#entry").on("click",function () {
-        console.log("got clicked");
-        // window.location = $(this).find("a").attr("href"); 
-        window.open("https://www.google.com/"); 
-    // window.open($(this).attr("href"), '_blank');
-  });
 
 
 
@@ -100,5 +100,24 @@ $(document).ready(function () {
   $("#clear-button").on("click", function () {
     $("#searchResults").empty();
   })
-  
+
+
+  $(".clicked").on("click", function () {
+    console.log("clicked");
+    var newClick = $(".main").attr("data-status");
+    console.log(newClick);
+
+    if (newClick === "open") {
+      $(".main").css("width", "100%");
+      $(".main").attr("data-status", "closed");
+      console.log( $(".main").attr("data-status"));
+    }
+    else {
+      $(".main").css("width", "85%");
+      $(".main").css("float", "right");
+
+      $(".main").attr("data-status", "open");
+      console.log( $(".main").attr("data-status"));
+    }
+  })
 });
